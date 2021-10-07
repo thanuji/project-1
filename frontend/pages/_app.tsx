d181dc4d5245b1c485f8e396d731a09882049e67
thanuji <tkwickramadhara@gmail.com>
2021-10-07 23:22:56 +0530

    install chakra

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react/dist/types/chakra-provider'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component />
+    </ChakraProvider>
+  );
 }
 export default MyApp
